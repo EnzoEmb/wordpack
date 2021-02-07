68917b64541960e85e5f151643431d9d710c668b
Enzo Vergara <enzoamb123@gmail.com>
2021-02-06 22:36:14 -0300

    move functions to one file

--- core/wordpack.php
+++ core/wordpack.php
@@ -18,6 +18,7 @@ function wordpack_disable_emojis() {
 add_action( 'init', 'wordpack_disable_emojis' );
 
 
+
 /**
  * 
  * Disable wp-embed.js
@@ -26,3 +27,43 @@ function wordpack_disable_embed_js(){
   wp_deregister_script( 'wp-embed' );
 }
 add_action( 'wp_footer', 'wordpack_disable_embed_js' );
+
+
+
+/**
+ * 
+ * Load the appropiate scripts for the specified route
+ */
+function wordpack_load_chunk($chunk_name){  
+  $chunks = file_get_contents( get_template_directory() . "/assets/chunks-manifest.json");
+  $chunks_json = json_decode($chunks, true);
+  $my_chunks = $chunks_json[$chunk_name]["scripts"];
+
+  $last_time_modified_manifest  = date("ymd-Gis", filemtime(get_template_directory() . '/assets/chunks-manifest.json'));
+
+
+  foreach ($my_chunks as $key => $value) {
+    wp_enqueue_script(
+      $chunk_name.'-'.$key,
+      get_template_directory_uri() . '/assets/'. substr($value, 2),
+      null,
+      $last_time_modified_manifest,
+      true
+    );
+  }
+}
+
+
+/**
+ * 
+ * Defer scripts
+ */
+function wordpack_defer_scripts($tag, $handle) {
+    $exclude = [];
+    if(!in_array($handle, $exclude)){
+      $tag = str_replace(' src', ' defer src', $tag);
+    }
+    return $tag;
+
+}
+add_filter('script_loader_tag', 'wordpack_defer_scripts', 10, 2);
--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php 
 include_once "core/wordpack.php";
-include_once "core/wordpack-functions.php";
 
 
 
